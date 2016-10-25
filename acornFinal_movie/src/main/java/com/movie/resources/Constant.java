01219595e27f69845036e68f1f3308f5774c1525
MAC <All@MACui-MacBook-Pro.local>
2016-10-25 10:37:51 +0900

    spring

---
+++ acornFinal_movie/src/main/java/com/movie/resources/Constant.java
@@ -0,0 +1,11 @@
+package com.movie.resources;
+
+public class Constant {
+	public static final String KOBIS_KEY = "fbfb0ceafa07f356234a048e8614c3a7";
+	public static final String DAUM_KEY = "&apikey=508518e65232b9d6a7130e696b6847d0";
+	public static final String DAUM_URL = "https://apis.daum.net/contents/movie";
+	public static final String DAUM_TYPE = "?output=json";
+	public static final String DAUM_URL_Q = "&q=";
+	public static final String DAUM_PAGE = "&pageno=";
+	public static final String DAUM_CNT = "&result=12";
+}
